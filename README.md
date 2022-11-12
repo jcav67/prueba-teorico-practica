e2fb71333ac296673924231ace6ee94a9fa583dd
Camilo Arango <jmilo67@hotmail.com>
2022-11-12 16:06:04 -0500

    archivos renombrados y reestructurados, readme agregado

---
+++ README.md
@@ -0,0 +1,8 @@
+# Prueba Tecnica
+
+> prueba tecnica realizada en python, cada uno de los ejercicios propuesto se resolvió en un archivo independiente, para el correcto funcionamiento del punto 1 el archivo que se lee debe tener el nombre file.txt y ubicarse en el mismo nivel del archivo orden_de_palabras.py, no se realizo el ejercicio relacionado a Odoo
+
+>Las preguntas tecnicas se encuentran en la carpeta "preguntas-tecnicas", cada sección se encuentra un archivo markdow nombrado con el lenguaje solicitado y siguiendo con la estructura pregunta->respuesta
+
+## por: Juan Camilo Arango Valle 
+## email: jmilo67@homtail.com
---
+++ fechas_y_horas_zonas_horarias.py
@@ -0,0 +1,70 @@
+import datetime
+import calendar
+import locale
+
+locale.setlocale(locale.LC_ALL, 'esp') 
+
+def conteo_de_dias(inicio, final) -> list:
+  """
+    mediante las librerias datetime y calendar se calcula la repeticion de dias 
+    de la semana entre 2 fechas suministradas por el usuario
+    args:
+      incio->String
+      Final->String
+    return
+      semana-> list
+  """
+  dia_inicial  = datetime.datetime.strptime(inicio,'%d/%m/%Y %H:%M:%S %z')
+  print(dia_inicial)
+  dia_final    = datetime.datetime.strptime(final,'%d/%m/%Y %H:%M:%S %z')
+  print(dia_final)
+  semana        = {}
+  for i in range((dia_final - dia_inicial).days + 1):
+    dia       = calendar.day_name[(dia_inicial + datetime.timedelta(days=i)).weekday()]
+    semana[dia] = semana[dia] + 1 if dia in semana else 1
+  return semana
+
+def horas_laboradas(inicio, final) -> int:
+  """
+    haciendo uso de la funcion conteo_de_dias creada anteriormente
+    se hace el conteo de dias nuevamente y se calculan las horas trabajadas
+    teniendo encuenta que todos los dias laborales se trabajaron completos
+    args:
+      incio->String
+      Final->String
+    return
+      horas_totales-> int
+  """
+  dias_laborados = conteo_de_dias(inicio,final)
+  dias_laborales = ['lunes', 'martes', 'miércoles', 'jueves', 'viernes']
+  horas_totales = 0
+
+  for dia in dias_laborales:
+    horas_totales += int(dias_laborados[dia]) * 8
+
+  return horas_totales
+  
+def diferencia_horas(inicio, final) -> dict:
+  """
+    mediante las librerias datetime y calendar se calcula la repeticion de dias 
+    de la semana entre 2 fechas suministradas por el usuario
+    args:
+      incio->String
+      Final->String
+    return
+      semana-> list
+  """
+  dia_inicial  = datetime.datetime.strptime(inicio,'%d/%m/%Y %H:%M:%S %z')
+  dia_final    = datetime.datetime.strptime(final, '%d/%m/%Y %H:%M:%S %z')
+  diferencia_dias = dia_final - dia_inicial
+
+  return {"segundos":diferencia_dias.seconds, "Horas":round(diferencia_dias.seconds/3600), "Días": diferencia_dias.days }
+
+
+fecha_1 ='10/10/2022 10:22:20 -1000'
+fecha_2 ='30/05/2023 19:42:20 +0930'
+print(conteo_de_dias(fecha_1, fecha_2))
+print("horas laboradas",horas_laboradas(fecha_1, fecha_2))
+print(diferencia_horas(fecha_1, fecha_2))
+
+
